406b4819d3c8043beb8e024076f17277c7ae16e5
Willian Soares da Silva <131213455+WillianSozz@users.noreply.github.com>
2023-09-01 04:15:05 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ export const Home = () => {
 
   const getPokemons = () => {
     var endpoints = [];
-    for (var i = 1; i < 1000; i++) {
+    for (var i = 1; i < 500; i++) {
       endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
     }
     axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => {
